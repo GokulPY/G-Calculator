fd4eef27107e7ff873e714382fa6f620b147fff1
GokulPY <gokulsparx05@gmail.com>
2023-02-17 10:57:31 +0530

    Html Css Js

---
+++ CalculatorG.js
@@ -0,0 +1,32 @@
+
+let buttons = document.querySelectorAll(".numbers");
+let displays = document.querySelector(".Display");
+let equals = document.querySelector(".Equalto");
+let clears = document.querySelector(".Clear");
+
+buttons.forEach((data)=>{
+    data.addEventListener("click",(e)=>{
+        let inputData = e.target.dataset.num;
+        displays.value+=inputData;
+    })
+});
+
+
+equals.addEventListener("click",(e)=>{
+    if(displays.value===""){
+        displays.value="";
+    }else{
+        let answer = eval(displays.value);
+        displays.value=answer;
+    }
+})
+
+clears.addEventListener("click",(e)=>{
+    displays.value="";
+})
+
+
+
+
+
+    
